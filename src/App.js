49bd728803849d69e3252b33aaa546091532398e
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-02-11 22:15:36 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { SyncLoader } from "react-spinners";
 
 import Form from "./components/Form";
 import Weather from "./components/Weather";
@@ -7,23 +7,45 @@ import Weather from "./components/Weather";
 import { getWeatherData } from "./weatherService";
 function App() {
     // states
-    const [city, setCity] = useState("srinagar");
+    const [loader, setLoader] = useState(false);
+    const [city, setCity] = useState(null);
     const [weather, setWeather] = useState(null);
 
     //fetch data from API
     useEffect(() => {
         const fetchData = async (city) => {
+            setLoader(true);
             const data = await getWeatherData(city);
-            setWeather(data);
+            if (data.ok === false) {
+                alert("There is some error!!! Try Again");
+                setLoader(false);
+            } else {
+                setWeather(data);
+                setLoader(false);
+            }
         };
-        fetchData(city);
+        if (city) {
+            fetchData(city);
+        }
     }, [city]);
-
     return (
         <div className="container">
             <div className="card">
-                <Form setCity={setCity} />
-                {weather && <Weather weather={weather} />}
+                <Form setCity={setCity} setLoader={setLoader} />
+                {weather ? (
+                    <Weather weather={weather} />
+                ) : (
+                    loader && (
+                        <SyncLoader
+                            color="#2f6ed3"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                marginTop: "10px",
+                            }}
+                        />
+                    )
+                )}
             </div>
         </div>
     );
