93d05e0aad05ce0d6e182a7a66ce0540bbc1a19e
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-02-11 22:28:17 +0530

    fix: API keys

--- src/weatherService.js
+++ src/weatherService.js
@@ -1,29 +1,34 @@
-const API_KEY = process.env.WEATHER_API_KEY;
+const API_KEY = process.env.REACT_APP_KEY;
 
 const makeIconUrl = (iconId) =>
     `https://openweathermap.org/img/wn/${iconId}@2x.png`;
 
 export const getWeatherData = async (city, units = "metric") => {
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
-    const response = await fetch(URL);
-    const data = await response.json();
-    const {
-        name,
-        weather,
-        main: { temp, feels_like, humidity, pressure },
-        wind: { speed },
-    } = data;
+    let response;
+    try {
+        const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+        response = await fetch(URL);
+        const data = await response.json();
+        const {
+            name,
+            weather,
+            main: { temp, feels_like, humidity, pressure },
+            wind: { speed },
+        } = data;
 
-    const { description, icon } = weather[0];
+        const { description, icon } = weather[0];
 
-    return {
-        name,
-        temp,
-        iconUrl: makeIconUrl(icon),
-        description,
-        feels_like,
-        humidity,
-        pressure,
-        speed,
-    };
+        return {
+            name,
+            temp,
+            iconUrl: makeIconUrl(icon),
+            description,
+            feels_like,
+            humidity,
+            pressure,
+            speed,
+        };
+    } catch (error) {
+        return response;
+    }
 };
