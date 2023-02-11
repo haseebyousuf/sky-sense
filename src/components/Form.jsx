32ca7a2ac49ab7ec0b09102e51989af01549ce0f
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-02-11 22:16:19 +0530

    added form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 
-const Form = ({ setCity }) => {
+const Form = ({ setCity, setLoader }) => {
     const handleSubmit = (e) => {
         e.preventDefault();
+        setLoader(true);
         setCity(e.target.city.value);
-        e.target.value = "";
+        e.target.city.value = "";
+        setLoader(false);
     };
     return (
         <form className="search" onSubmit={handleSubmit}>
